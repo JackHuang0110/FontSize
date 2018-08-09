72599335e15935e731ac553e5d507625ee89b49f
Jack Huang <jackhuang@Jackde-MacBook-Air.local>
2018-08-09 16:59:11 +0800

    new project

--- FontSize/ViewController.swift
+++ FontSize/ViewController.swift
@@ -9,12 +9,39 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var fontSize : Float = 0.0
+    
+    @IBOutlet weak var showTextField: UITextField!
+    @IBOutlet weak var fontLenghLabel: UILabel!
+    @IBOutlet weak var fontSizeSlider: UISlider!
+    @IBOutlet weak var fontSizeLabel: UILabel!
+    
+    @IBAction func fontSizeSliderChanged(_ sender: UISlider) {
+        
+        //取得目前字體
+        let font = showTextField.font!
+        fontSize = sender.value
+        //數字變字串顯示到小數點第二位
+        fontSizeLabel.text = String(format: "%.2f", fontSize)
+        //指定字體大小 & Float 變 CGFloat
+        showTextField.font = font.withSize(CGFloat(fontSize))
 
+    }
+    @IBAction func entryTextField(_ sender: UITextField) {
+        fontSizeLabel.text = String(fontSizeSlider.value)
+        //確認字數
+        fontLenghLabel.text = String(showTextField.text!.count)
+    }
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    
+    
 }
 
